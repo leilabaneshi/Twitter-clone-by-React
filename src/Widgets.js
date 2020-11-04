3b120cf7b938adc5d7a509f8ce66d2564c7e62db
Leila Baneshi <baneshi.leila@gmail.com>
2020-11-04 23:35:06 +0100

    create widgets copmonent

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Widgets.css';
+import SearchIcon from '@material-ui/icons/Search';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from 'react-twitter-embed';
 
 function Widgets() {
     return (
         <div className='widgets'>
-            <h1> widgets</h1>
+            <div className="widgets__input">
+                <SearchIcon className='widgets__searchIcon' />
+                <input type="text" placeholder='Search Twitter' />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>What is happening</h2>
+                <TwitterTweetEmbed tweetId={'858551177860055040'} />
+
+                <TwitterTimelineEmbed
+                    sourceType='profile'
+                    screenName='cleverqazi'
+                    options={{ height: 400 }} />
+                <TwitterShareButton
+                    url={'https://facebook.com/cleverprogrammer'}
+                    options={{ text: '#reactjs is awesome', via: 'clevergazi' }} />
+            </div>
         </div>
     )
 }
